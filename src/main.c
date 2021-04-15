c78198a58110ec2805d226190f5dc3954478ff18
wangliang <wangliang199538@live.com>
2021-04-15 14:54:12 +0800

    start parse command line opotions

--- src/main.c
+++ src/main.c
@@ -1,7 +1,40 @@
+#include <stdlib.h>
 #include "main.h"
+#include "string.h"
+#include "stdio.h"
 
-int main(){
+int main(int argc, char** argv){
 
+    //start schedule task
+    if (argc == 1){
+
+        return 0;
+    }
+
+    argc--; argv++;
+
+    /* Parse command line options. */
+    while (argc > 0){
+
+        if (argc == 1 && !strcmp(argv[0], "show")) {
+            //show list
+        } else if (argc >= 2 && !strcmp(argv[0], "-n")){
+
+        } else if (argc >= 2 && !strcmp(argv[0], "-p")){
+
+        } else if (argc >= 2 && !strcmp(argv[0], "-r")){
+
+        } else if (argc >= 2 && !strcmp(argv[0], "-d")){
+
+        } else if (argc >= 2 && !strcmp(argv[0], "-n")){
+
+        } else{
+            fprintf(stderr, "Invalid argument %s \n", argv[0]);
+            exit(1);
+        }
+
+        argc--; argv++;
+    }
 
 
     return 0;
