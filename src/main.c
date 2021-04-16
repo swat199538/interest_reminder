11d816cec52836a3a9dd6c579c0f67be41a03b22
wangliang <wangliang199538@live.com>
2021-04-16 11:03:08 +0800

    parse arguement

--- src/main.c
+++ src/main.c
@@ -11,24 +11,56 @@ int main(int argc, char** argv){
         return 0;
     }
 
+    struct iConfig config = {
+            .rate = 0,
+            .name = "",
+            .d_time =0,
+            .money =0,
+            .p_day=0,
+            .s_date=0
+    };
+
     argc--; argv++;
 
     /* Parse command line options. */
     while (argc > 0){
 
-        if (argc == 1 && !strcmp(argv[0], "show")) {
+        if (argc == 1 && !strcmp(argv[0], "show"))
+        {
+
+
             //show list
-        } else if (argc >= 2 && !strcmp(argv[0], "-n")){
-
-        } else if (argc >= 2 && !strcmp(argv[0], "-p")){
-
-        } else if (argc >= 2 && !strcmp(argv[0], "-r")){
-
-        } else if (argc >= 2 && !strcmp(argv[0], "-d")){
-
-        } else if (argc >= 2 && !strcmp(argv[0], "-n")){
-
-        } else{
+        } else if (argc >= 2 && !strcmp(argv[0], "-n"))
+        {
+            argc--;argv++;
+            config.name = argv[0];
+        } else if (argc >= 2 && !strcmp(argv[0], "-m"))
+        {
+            argc--;argv++;
+            char *ptr;
+            config.money = strtod(argv[0], &ptr);
+        } else if (argc >= 2 && !strcmp(argv[0], "-s"))
+        {
+            argc--;argv++;
+            char *ptr;
+            config.s_date = strtol(argv[0], &ptr, 32);
+        } else if (argc >= 2 && !strcmp(argv[0], "-d"))
+        {
+            argc--;argv++;
+            char *ptr;
+            config.p_day = strtol(argv[0], &ptr, 32);
+        } else if (argc >= 2 && !strcmp(argv[0], "-r"))
+        {
+            argc--;argv++;
+            char *ptr;
+            config.rate = strtod(argv[0], &ptr);
+        } else if (argc >= 2 && !strcmp(argv[0], "-c")){
+            argc--;argv++;
+            char *ptr;
+            config.d_time = strtol(argv[0], &ptr, 32);
+        }
+        else
+        {
             fprintf(stderr, "Invalid argument %s \n", argv[0]);
             exit(1);
         }
@@ -36,6 +68,17 @@ int main(int argc, char** argv){
         argc--; argv++;
     }
 
+    char *err = "";
+
+    if (check_config(config, err) == 0){
+        fprintf(stderr, "%s", err);
+        exit(1);
+    }
+
+    return 0;
+}
+
+int check_config(struct iConfig config, char* err){
 
     return 0;
 }
--- src/main.h
+++ src/main.h
@@ -2,3 +2,26 @@
 #define INTEREST_REMINDER_MAIN_H
 
 #endif //INTEREST_REMINDER_MAIN_H
+
+typedef struct iConfig{
+    // The name of the account
+    char * name;//
+
+    // amount deposited
+    double money;//
+
+    // deposit interest rate
+    double rate;
+
+    // Deposit time(days)
+    long d_time;
+
+    // payment of interest duration(days)
+    long p_day;//
+
+    // start time
+    long s_date;//
+} iConfig;
+
+
+int check_config(struct iConfig config, char* err);
