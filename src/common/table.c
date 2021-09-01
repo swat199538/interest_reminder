754a6eb1345077ef695eedd5ab27af1d0d46d281
wangliang <wangliang199538@live.com>
2021-09-01 17:58:47 +0800

    temp save

--- src/common/table.c
+++ src/common/table.c
@@ -1,9 +1,9 @@
-//
-// Created by wangl on 8/28/2021.
-//
-
 #include "table.h"
 
+#define CORNER_FLAG "+"
+#define ROW_LINE "_"
+#define COL_PADDING " "
+#define COL_LINE "|"
 
 static int str_len(unsigned char* str){
     int len =0;
@@ -22,7 +22,16 @@ static int str_len(unsigned char* str){
     return len;
 }
 
+static  void make_edge(const unsigned  int *col_width, const unsigned  int col_num, char *edge){
+    memset(edge, 0, sizeof (edge));
+    strcat(edge, CORNER_FLAG);
+    int col_line_counter, row_line_counter;
 
+    for(col_line_counter=0; col_line_counter < col_num; col_line_counter++){
+        strcat(edge, ROW_LINE)
+    }
+
+}
 
 void format_table(table *t, char* *formatted_out){
 
--- src/test/table_test.c
+++ src/test/table_test.c
@@ -3,3 +3,17 @@
 //
 
 #include "table_test.h"
+#include "stdio.h"
+
+int main(int argc, char **argv){
+
+    char *str = "this is a str";
+
+    int a = sizeof(str);
+    int b = sizeof(*str);
+
+    printf("a is %d\n", a);
+    printf("b is %d\n", b);
+
+    return 0;
+}
